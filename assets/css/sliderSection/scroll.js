5627e9e8d03fce96897fb984cb9eba987c67fac1
Tran Mai Nhung <nhungmaitran1412@gmail.com>
2022-03-12 14:31:54 +0700

    scroll fix done

--- assets/css/sliderSection/scroll.js
+++ assets/css/sliderSection/scroll.js
@@ -4,11 +4,13 @@ slideScrollStyle = new SlideObject("/assets/img/slider/sliderScroll.jpeg",
     "radial-gradient(circle, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 30%, rgba(10,20,41,0.9024203431372549) 74%)",
     "リムル゠テンペスト","");
 
+const defaultBackgroundSize = "0% 0%";
+
 let slideScroll = document.createElement("div");
-slideScroll.setAttribute("class", "slide scroll-fade");
+slideScroll.setAttribute("class", "slide");
 slideScroll.setAttribute("id", "slide-scroll");
 slideScroll.style.background = " " + slideScrollStyle;
-slideScroll.style.backgroundSize = "30% 30%";
+slideScroll.style.backgroundSize = defaultBackgroundSize;
 
 let slideScrollHeader = document.createElement("h3");
 slideScrollHeader.setAttribute("class", "slide-scroll__header");
@@ -28,8 +30,20 @@ function scrollDown() {
     let newY = 0;
     window.addEventListener("scroll", (e) => {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            slideScroll.style.backgroundSize = "100% 100%";
+            slideScroll.style.backgroundSize = "150% 150%";
+            slideScroll.style.filter = "brightness(75%)";
+            slideScroll.style.opacity = "0.4";
+            slideScroll.style.zIndex = "2";
+
+            // cannot use classList because .style have higher predence
+            // slideScroll.classList.add("scroll-fade");
         }
-        
+        else {
+            slideScroll.style.backgroundSize = defaultBackgroundSize;
+            slideScroll.style.filter = "brightness(100%)";
+            slideScroll.style.zIndex = "1";
+            slideScroll.style.opacity = "1";
+            // slideScroll.classList.remove("scroll-fade");
+        }
     });
 }
--- assets/css/sliderSection/sliding.js
+++ assets/css/sliderSection/sliding.js
@@ -8,74 +8,75 @@ function SlideObject(imgUrl, gradient, h3, p) {
     // override toString
     SlideObject.prototype.toString = function() {
         return this.gradient + 'center / cover  no-repeat,' + 'url("' + this.imgUrl + '") center / cover  no-repeat';
+        // return this.gradient + ', url("' + this.imgUrl + '")';
     };
 }
-// // declare object
-// const arr = [
-//     new SlideObject('/assets/img/slider/slider1.jpeg',
-//             "radial-gradient(circle, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 30%, rgba(10,20,41,0.9024203431372549) 74%)",
-//             "Rimuru Tempest",
-//             "Main, a slime, and a previous human"),
-//     new SlideObject('/assets/img/slider/slider2.jpeg',
-//             "radial-gradient(circle, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 40%, rgba(10,20,41,0.9024203431372549) 97%)",
-//             "Rimuru And Friend",
-//             "King and government of Jura Tempest Federation"),
-//     new SlideObject('/assets/img/slider/slider3.jpeg',
-//             "linear-gradient(40deg, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 46%, rgba(10,20,41,0.9024203431372549) 88%)",
-//             "Jura Tempest Federation",
-//             "View of Monster Federation viewed from sky")
-// ];
+// declare object
+const arr = [
+    new SlideObject('/assets/img/slider/slider1.jpeg',
+            "radial-gradient(circle, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 30%, rgba(10,20,41,0.9024203431372549) 74%)",
+            "Rimuru Tempest",
+            "Main, a slime, and a previous human"),
+    new SlideObject('/assets/img/slider/slider2.jpeg',
+            "radial-gradient(circle, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 40%, rgba(10,20,41,0.9024203431372549) 97%)",
+            "Rimuru And Friend",
+            "King and government of Jura Tempest Federation"),
+    new SlideObject('/assets/img/slider/slider3.jpeg',
+            "linear-gradient(40deg, rgba(241,227,142,0.4038209033613446) 0%, rgba(63,143,116,0.4038209033613446) 46%, rgba(10,20,41,0.9024203431372549) 88%)",
+            "Jura Tempest Federation",
+            "View of Monster Federation viewed from sky")
+];
 
 
-// let parrent = document.getElementById("slider");
-// for (let aSlideStyle of arr) {
-//     let aSlide = document.createElement("div");
+let parrent = document.getElementById("slider");
+for (let aSlideStyle of arr) {
+    let aSlide = document.createElement("div");
     
-//     // this method for adding only 1 class name
-//     // aSlide.className = "js__slide";
-//     aSlide.setAttribute("class", "js__slide slide fade");
-//     aSlide.style.background = " " + aSlideStyle;
+    // this method for adding only 1 class name
+    // aSlide.className = "js__slide";
+    aSlide.setAttribute("class", "js__slide slide fade");
+    aSlide.style.background = " " + aSlideStyle;
 
 
-//     let aSlideContent = document.createElement("div");
-//     aSlideContent.setAttribute("class", "slide__content");
+    let aSlideContent = document.createElement("div");
+    aSlideContent.setAttribute("class", "slide__content");
     
-//     let aSlideContentHeader = document.createElement("h3");
-//     aSlideContentHeader.setAttribute("class", "slide__content-header");
-//     aSlideContentHeader.innerHTML = aSlideStyle.h3;
+    let aSlideContentHeader = document.createElement("h3");
+    aSlideContentHeader.setAttribute("class", "slide__content-header");
+    aSlideContentHeader.innerHTML = aSlideStyle.h3;
     
-//     let aSlideContentText = document.createElement("p");
-//     aSlideContentText.setAttribute("class", "slide__content-text");
-//     aSlideContentText.innerHTML = aSlideStyle.p;
+    let aSlideContentText = document.createElement("p");
+    aSlideContentText.setAttribute("class", "slide__content-text");
+    aSlideContentText.innerHTML = aSlideStyle.p;
     
-//     aSlideContent.appendChild(aSlideContentHeader);
-//     aSlideContent.appendChild(aSlideContentText);
-//     aSlide.appendChild(aSlideContent);
-//     parrent.prepend(aSlide);
-//     // document.getElementById("slider").appendChild(aSlide);
-//     // $(".js__slider").appendChild(aSlide);
+    aSlideContent.appendChild(aSlideContentHeader);
+    aSlideContent.appendChild(aSlideContentText);
+    aSlide.appendChild(aSlideContent);
+    parrent.prepend(aSlide);
+    // document.getElementById("slider").appendChild(aSlide);
+    // $(".js__slider").appendChild(aSlide);
 
-// }
+}
 
 
 
-// // for changing slides on slider
-// let slideIndex = 0;
-// changeSlide();
+// for changing slides on slider
+let slideIndex = 0;
+changeSlide();
 
-// function changeSlide() {
-//     let slides = document.getElementsByClassName('js__slide');
+function changeSlide() {
+    let slides = document.getElementsByClassName('js__slide');
 
-//     // undisplay all slides b/f displaying next slide
-//     for (i = 0; i < slides.length; i ++) 
-//         slides[i].style.display = "none";
+    // undisplay all slides b/f displaying next slide
+    for (i = 0; i < slides.length; i ++) 
+        slides[i].style.display = "none";
 
-//     // change to next slide
-//     slideIndex ++;
-//     if (slideIndex >= slides.length) slideIndex = 0;
+    // change to next slide
+    slideIndex ++;
+    if (slideIndex >= slides.length) slideIndex = 0;
 
-//     slides[slideIndex].style.display = "block";
+    slides[slideIndex].style.display = "block";
 
-//     // 2s for changing slide
-//     setTimeout(changeSlide, 2000);
-// }
+    // 2s for changing slide
+    setTimeout(changeSlide, 2000);
+}
